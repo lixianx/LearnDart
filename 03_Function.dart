6b8524df7f177aad83ec683f711a004d794d0938
lixianx xing <lixian666@mail.ynu.edu.cn>
2023-04-03 11:10:26 +0000

    Variable and Function

---
+++ 03_Function.dart
@@ -0,0 +1,19 @@
+import '01_Variables.dart';
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+var result = fibonacci(20);
+
+void main() {
+  print(fibonacci(20));
+
+  var nums = [0, 1, 2, 3, 4, 5];
+  var nn = nums.map((num) => num * 2);
+  nn.forEach(print);
+
+  //筛选出含有字符串turn的元素并将其输出
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
+}
